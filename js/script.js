f8fda24cb077f148df2fe2c094910dc8ef2db7c4
ayraa34 <ggg446756@gmail.com>
2025-07-11 23:22:26 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let typed = new Typed(".teks", {
+    strings: ["Sedang Mengerjakan Projek Uts", "Sistem Informasi "],
+    typeSpeed: 130,
+    backSpeed: 60,
+    backDelay: 1000,
+    loop: true,
+  });
